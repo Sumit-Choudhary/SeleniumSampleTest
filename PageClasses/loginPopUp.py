c9b725578b82a19d4af23abee126fe62f10be98c
Sumit-Choudhary <47511039+Sumit-Choudhary@users.noreply.github.com>
2020-03-09 08:20:52 +0530

    Sample test method to login

--- PageClasses/loginPopUp.py
+++ PageClasses/loginPopUp.py
@@ -5,16 +5,16 @@ class flipkartMainLoginPopOver:
     URL = 'https://www.flipkart.com'
 
     def __init__(self, driver):
-        self.driver = webdriver.Firefox()
+        self.driver = driver
 
     def loadFlipKart(self):
         self.driver.get(self.URL)
 
     def loginToFlipKart(self, usernamevalue, password):
-        userNameInputBox = self.driver.find_element_by_css_selector(self, "[class='_2zrpKA _1dBPDZ']")
-        passwordInputBox = self.driver.find_element_by_css_selector(self, "[class='_2zrpKA _3v41xv _1dBPDZ']")
-        loginbutton = self.driver.find_element_by_css_selector(self, "[class='_2AkmmA _1LctnI _7UHT_c']")
+        userNameInputBox = self.driver.find_element_by_css_selector("[class='_2zrpKA _1dBPDZ']")
+        passwordInputBox = self.driver.find_element_by_css_selector("[class='_2zrpKA _3v41xv _1dBPDZ']")
+        loginbutton = self.driver.find_element_by_css_selector("[class='_2AkmmA _1LctnI _7UHT_c']")
 
-        userNameInputBox.send_keys(self, usernamevalue)
-        passwordInputBox.send_keys(self, password)
-        loginbutton.click(self)
+        userNameInputBox.send_keys(usernamevalue)
+        passwordInputBox.send_keys(password)
+        loginbutton.click()
--- TestClasses/SampleTest.py
+++ TestClasses/SampleTest.py
@@ -1,16 +1,18 @@
 from PageClasses import loginPopUp
+import unittest
+from selenium import webdriver
 
 
 def login():
-    loginPopUp.flipkartMainLoginPopOver.loadFlipKart()
-    loginPopUp.flipkartMainLoginPopOver.loginToFlipKart('8939667714', '')
+    v = loginPopUp.flipkartMainLoginPopOver(webdriver.Firefox())
+    v.loadFlipKart()
+    v.loginToFlipKart('8939667714', 'cometome')
 
 
-class ValidateFlipKartLogin:
+class ValidateFlipKartLogin(unittest.TestCase):
+    def testlogin(self):
+        login()
+        self.assertEqual(1==1)
 
-    def __init__(self):
-       self.loginPopUp = loginPopUp
-
-
-
-
+    if __name__ == '__main__':
+        unittest.main()
